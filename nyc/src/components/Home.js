79bfbcce491f036ccf4054a117fe198b1f455d8c
Dominique Clark <melaydominique@gmail.com>
2023-07-27 18:22:26 -0400

    menu bar functionality added. usestate, onclick event

--- nyc/src/components/Home.js
+++ nyc/src/components/Home.js
@@ -1,6 +1,13 @@
 import {Link} from 'react-router-dom'
+import {useState, useEffect } from 'react'
 
 const Home = () => {
+    const [menu, setMenu] = useState(false)
+
+    const handleMenu = () => {
+        setMenu(!menu)
+    }
+
 
 
     return (
@@ -20,7 +27,7 @@ const Home = () => {
                         <h2 className='address'>203 East 26th St</h2>
                         <h2 className='address'>Kips Bay, NYC 10010</h2>
                     </div>
-                    <div className='logodiv'>
+                    <div onClick= {handleMenu} className='logodiv'>
                         <img className='logo' src='/images/pinelogo.png' alt='pineapplenyc logo'/>
                     </div>
                 </div>
@@ -33,14 +40,25 @@ const Home = () => {
 
             </div>
 
-            <div className='navcontainer'>
+            { menu ? 
+
+            <div onClick = {handleMenu} className='navcontainer'>
                 <div className='nav'>
                     <Link to = {'/'}><h3 className='navlink'>Home</h3></Link>
                      <Link to = {'/about'}><h3 className='navlink'>About</h3></Link>
+
+                     <h3 className='navlink'>Contact</h3>
+                     <h3 className='navlink'>Vendors</h3>
+                     <h3 className='navlink'>Lookbook</h3>
+                     <h3 className='navlink'>Events</h3>
                     
                 </div>
             </div>
-        
+
+            :
+            <></>
+
+            }
 
         </div>
         </>
---
+++ nyc/src/components/Menu.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+import {useState, useEffect } from 'react'
+import React from 'react'
+
+
+
+const Menu = () => {
+
+
+
+
+    return (
+        <>
+        
+        
+        
+        </>
+    )
+}
