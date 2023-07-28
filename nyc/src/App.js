aae507b60ce4187091006d163fc711a51205ed7e
Dominique Clark <melaydominique@gmail.com>
2023-07-28 14:11:11 -0400

    basename error. On click event for menu display not functional. Div not clickable, look for styling inconsistencies.functionality first approach

--- nyc/src/App.js
+++ nyc/src/App.js
@@ -3,14 +3,50 @@ import './App.css'
 import Home from './components/Home'
 import About from './components/About'
 import {Route, Routes, BrowserRouter, Link} from 'react-router-dom'
+import{useState, useEffect} from 'react'
+
+
 const App = () => {
+  const [menu, setMenu] = useState(false)
 
-
-
+  const handleMenu = () => {
+    setMenu(true)
+    // alert('clicked');
+  }
 
   return (
 
    <>
+
+    <div onClick = {handleMenu} className='appcontainer'>
+
+      <div onClick = {handleMenu} className='menudiv'>
+        <img src='/images/menu.png' alt='menu bar' className='menubar'/>
+      </div>
+
+{ menu ? 
+
+<div onClick = {handleMenu} className='navcontainer'>
+    <div className='nav'>
+        <Link to = {'/'}><h3 className='navlink'>Home</h3></Link>
+        <Link to = {'/about'}><h3 className='navlink'>About</h3></Link>
+
+         <h3 className='navlink'>Contact</h3>
+         <h3 className='navlink'>Vendors</h3>
+         <h3 className='navlink'>Lookbook</h3>
+         <h3 className='navlink'>Events</h3>
+        
+    </div>
+</div>
+
+:
+
+<></>
+
+}
+
+
+
 
     <BrowserRouter>
       <Routes>
@@ -21,6 +57,8 @@ const App = () => {
       </Routes>
     </BrowserRouter>
 
+
+    </div>
    </>
 
   )
--- nyc/src/components/Home.js
+++ nyc/src/components/Home.js
@@ -2,11 +2,6 @@ import {Link} from 'react-router-dom'
 import {useState, useEffect } from 'react'
 
 const Home = () => {
-    const [menu, setMenu] = useState(false)
-
-    const handleMenu = () => {
-        setMenu(!menu)
-    }
 
 
 
@@ -17,11 +12,7 @@ const Home = () => {
                     
     <div className='container'>
 
-    <div onClick= {handleMenu} className='logodiv'>
-                        {/* <img className='logo' src='/images/pinelogo.png' alt='pineapplenyc logo'/> */}
-
-                        <img src='/images/menu.png' alt='menu icon' className='menubar'/>
-                        </div>
+ 
             <div className='homecontainer'>
 
         <div className='homediv1'>
@@ -48,25 +39,7 @@ const Home = () => {
 
             </div>
 
-            { menu ? 
-
-            <div onClick = {handleMenu} className='navcontainer'>
-                <div className='nav'>
-                    <Link to = {'/'}><h3 className='navlink'>Home</h3></Link>
-                     <Link to = {'/about'}><h3 className='navlink'>About</h3></Link>
-
-                     <h3 className='navlink'>Contact</h3>
-                     <h3 className='navlink'>Vendors</h3>
-                     <h3 className='navlink'>Lookbook</h3>
-                     <h3 className='navlink'>Events</h3>
-                    
-                </div>
-            </div>
-
-            :
-            <></>
-
-            }
+       
 
         </div>
         </>
